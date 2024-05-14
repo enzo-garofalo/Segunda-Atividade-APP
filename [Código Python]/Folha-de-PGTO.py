d13f9b7a6d617408372e968d61a21019385af729
RogerioMedina <roger.m2@puccampinas.edu.br>
2024-05-13 22:09:51 -0300

    Alteracoes 1305

--- [Código Python]/Folha-de-PGTO.py
+++ [Código Python]/Folha-de-PGTO.py
@@ -1,4 +1,5 @@
-#Atividade Avaliativa 2 - Testes
+#Atividade Avaliativa 2
+
 import os
 from prettytable import PrettyTable
 
@@ -17,20 +18,23 @@ def cadastrar():
 
         if Funcao == 101:
             salario_bruto = 1500.00
+            valor_vendas = float(input("Digite o valor total das vendas: R$"))
+
         elif Funcao == 102:
             while True:
                 salario_bruto = float(input("Digite o salário bruto: (Limite inferior: 2150.00 | Limite superior: 6950.00). Salário: "))
+                valor_vendas = 0
                 if salario_bruto < 2150 or salario_bruto > 6950:
                     print("Digite uma opção válida. (Limite inferior: 2150.00 | Limite superior: 6950.00)")
                     continue
                 else:
                     break
         else:
-            print("Digite uma opção válida. 101 - vendedor | 102 Administrativo")
+            print("Digite uma opção válida. 101 - Vendedor | 102 - Administrativo")
             continue
         num_faltas = int(input(f"Digite o numero de faltas do funcionário {Nome}: "))
         
-        funcionarios[Matricula] = [Nome, Funcao, salario_bruto, num_faltas ]
+        funcionarios[Matricula] = [Nome, Funcao, salario_bruto, num_faltas, valor_vendas ]
 
         print(funcionarios)
         n_cadastro = int(input("Deseja cadastrar outro funcionário? 1 (sim) | 2 (não). R: "))
@@ -56,12 +60,33 @@ def consultar():
 
 def relatorio_financeiro():
 
-    vendas_mensal = 0 #como será adicionada a venda mensal / deve ser venda por func ou por mês
+    while True:
+
+        Matricula = int(input("Qual maltrícula do funionário que deseja fazer o cálculo: "))
+
+        nome = funcionarios[Matricula][0]
+        funcao = funcionarios[Matricula][1]
+        salario_bruto = funcionarios[Matricula][2]
+        num_faltas = funcionarios[Matricula][3]
+        vendas_mensal = funcionarios[Matricula][4]
+        salario_liquido = 0  #FAZER O CÁLCULO
+        desconto_falta = (salario_bruto/30)*num_faltas
+        percentual_imposto = 0
+
+        tabela = PrettyTable(["Matrícula", "Nome", "Função", "Salário Bruto", "Salario Líquido", "Número de faltas", "Percentual imposto", "Desconto"])
+
+        tabela.add_row([Matricula, nome, funcao, f"R$ {salario_bruto:.2f}", f"R$ {salario_liquido:.2f}", num_faltas, f"{percentual_imposto}%", f"{desconto_falta:.2f}"])
+        
+        print(tabela)
+
+        nova_consulta = int(input("Deseja fazer nova consulta? 1 (sim) | 2 (não). R: "))
+        
+        if nova_consulta == 1:
+            continue
+        else:
+            return
+
     
-    return()
-
-
-
 
 def menu():
     while True:
@@ -70,7 +95,8 @@ def menu():
         print('-'*6, '| 1- Cadastrar |', '-'*6)
         print('-'*6, '| 2- Consultar |', '-'*6)
         print('-'*6, '| 3- Remover   |', '-'*6)
-        print('-'*6, '| 4- Sair      |', '-'*6)
+        print('-'*6, '| 4- Relatório |', '-'*6)
+        print('-'*6, '| 5- Sair      |', '-'*6)
         print('='*30)
         escolha = int(input("O que deseja fazer? "))
         if escolha == 1:
@@ -80,6 +106,8 @@ def menu():
         elif escolha == 3:
             remover()
         elif escolha == 4:
+            relatorio_financeiro()
+        elif escolha == 5:
             print('='*12,'ATÉ LOGO!','='*12)
             exit()
         else:
